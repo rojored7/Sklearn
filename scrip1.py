5347309f175a58f5f5a10bd17e285e680e15ac08
Rojo <u1802563@gmail.com>
2024-02-29 16:05:16 -0500

    Update(scri1):sklearn graphs

--- scrip1.py
+++ scrip1.py
@@ -31,3 +31,42 @@ if __name__ == "__main__":
   
    # Partimos el conjunto de entrenamiento. Para añadir replicabilidad usamos el random state
    X_train, X_test, y_train, y_test = train_test_split(dt_features, dt_target, test_size=0.3, random_state=42)
+   # Consultamos la fórmula para nuestra tabla
+   print(X_train.shape)
+   print(y_train.shape)
+   
+# Llamamos y configuramos nuestro algoritmo pca
+   '''EL número de componentes es opcional, ya que por defecto si no le pasamos el número de componentes lo asignará de esta forma:
+   a: n_components = min(n_muestras, n_features)'''
+   pca = PCA(n_components=3)
+   # Esto para que nuestro PCA se ajuste a los datos de entrenamiento que tenemos como tal
+   pca.fit(X_train)
+   #Como haremos una comparación con incremental PCA, haremos lo mismo para el IPCA.
+   '''EL parámetro batch se usa para crear pequeños bloques, de esta forma podemos ir entrenandolos
+   poco a poco y combinarlos en el resultado final'''
+   ipca = IncrementalPCA(n_components=3, batch_size=10)
+   #Esto para que nuestro PCA se ajuste a los datos de entrenamiento que tenemos como tal
+   ipca.fit(X_train)
+   ''' Aquí graficamos los números de 0 hasta la longitud de los componentes que me sugirió el PCA o que
+   me generó automáticamente el pca en el eje x, contra en el eje y, el valor de la importancia
+   en cada uno de estos componentes, así podremos identificar cuáles son realmente importantes
+   para nuestro modelo '''
+   plt.plot(range(len(pca.explained_variance_)), pca.explained_variance_ratio_)
+   plt.show()
+   #Ahora vamos a configurar nuestra regresión logística
+   logistic = LogisticRegression(solver='lbfgs')
+   # Configuramos los datos de entrenamiento
+   dt_train = pca.transform(X_train)
+   dt_test = pca.transform(X_test)
+   # Mandamos los data frames la la regresión logística
+   logistic.fit(dt_train, y_train)
+   #Calculamos nuestra exactitud de nuestra predicción
+   print("SCORE PCA: ", logistic.score(dt_test, y_test))
+   #Configuramos los datos de entrenamiento
+   dt_train = ipca.transform(X_train)
+   dt_test = ipca.transform(X_test)
+   # Mandamos los data frames la la regresión logística
+   logistic.fit(dt_train, y_train)
+   #Calculamos nuestra exactitud de nuestra predicción
+   print("SCORE IPCA: ", logistic.score(dt_test, y_test))
+   
